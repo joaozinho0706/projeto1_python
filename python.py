91d00d60578b51fec58e447b7182828ec6fea203
joaozinho0706 <126591273+joaozinho0706@users.noreply.github.com>
2025-02-18 09:55:33 -0300

    Add files via upload

--- python.py
+++ python.py
@@ -1,14 +1,19 @@
-print('fast food\n')
+import os
 
-print('1. cadastrar restaurante')
-print('2. listar restaurante')
-print('3. ativar restaurante')
-print('4. sair')
+def exiber_nome_do_programa():
+    print('fast food\n')
+
+def exibir_opcoes():
+    print('1. cadastrar restaurante')
+    print('2. listar restaurante')
+    print('3. ativar restaurante')
+    print('4. sair \n')
 
 opcao_escolhida =int(input('Escolha uma opção: '))
 print(f'Voce escolheu a opcao {opcao_escolhida}')
 
 def finalizar_app():
+    os.system('cls')
     print('finalizar o app')
 
 if opcao_escolhida == 1:
@@ -18,4 +23,10 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:
     print('Ativar restaurante')
 else:
-    finalizar_app
+    finalizar_app()
+
+def main():
+    exiber_nome_do_programa()
+    exibir_opcoes
+
+if __name__ == '__main__':
